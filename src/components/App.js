1900f3ac968f208638f8c24eac9f5ed0393ab4ca
Roman <RomanLutsenko2001@gmail.com>
2024-02-25 15:52:42 +0700

    lesson1_1

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,44 @@
-function App() {
-	return <div className='App'>asd</div>
+import { Component } from 'react'
+import Header from './Header'
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = { value: 0 }
+	}
+
+	componentDidMount() {
+		return console.log('DidMount')
+	}
+
+	componentDidUpdate() {
+		return console.log('DidUpdate')
+	}
+
+	componentWillUnmount() {
+		return console.log('WillUnmount')
+	}
+
+	handleInputChange = e => {
+		this.setState({ value: e.target.value })
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		console.log(this.state.value)
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<Header value={this.state.value} />
+				<form action='' onSubmit={this.handleSubmit}>
+					<input type='text' onChange={this.handleInputChange} />
+					<button>Кнопка</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default App
